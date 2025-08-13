664abc1bd2e57c04b6d5b484ffcfdcf4279d7142
Taham550 <tahammalik216@gmail.com>
2025-08-13 17:04:42 +0530

    navbar and delete option add

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from datetime import datetime
 app = Flask(__name__)
 DB_NAME = 'expenses.db'
 
-
 def init_db():
     with sqlite3.connect(DB_NAME) as conn:
         conn.execute('''CREATE TABLE IF NOT EXISTS expenses (
@@ -16,7 +15,6 @@ def init_db():
                     )''')
         conn.commit()
 
-
 def get_db_connection():
     conn = sqlite3.connect(DB_NAME)
     conn.row_factory = sqlite3.Row  
@@ -24,12 +22,10 @@ def get_db_connection():
 
 init_db()
 
-date = datetime.now().date()
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         category = request.form['category']
         amount = request.form['amount']
 
@@ -42,16 +38,23 @@ def index():
 
         return redirect('/')
 
-  
     with get_db_connection() as conn:
         expenses = conn.execute(
-            "SELECT date, category, amount FROM expenses ORDER BY date DESC"
+            "SELECT date, category, amount FROM expenses ORDER BY date ASC"
         ).fetchall()
 
         total = conn.execute("SELECT SUM(amount) FROM expenses").fetchone()[0] or 0
 
     return render_template('index.html', expenses=expenses, total=total)
 
+
+
+@app.route('/delete_all', methods=['POST'])
+def delete_all():
+    with get_db_connection() as conn:
+        conn.execute("DELETE FROM expenses")
+        conn.commit()
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
